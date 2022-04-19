eea9c4c731a82d4646f572071a1a1db75bb96722
Federico Grandi <fgrandi30@gmail.com>
2022-04-19 19:55:46 +0200

    chore: config repo

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+const config = {
+  preset: 'ts-jest',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+};
+
+export default config;
